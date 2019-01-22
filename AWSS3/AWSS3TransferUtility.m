200be5b0eb4e93902f95f36c2061b82236c0c2b1
Ryan Romanchuk <rromanchuk@gmail.com>
2019-01-21 19:23:17 -0800

    log the task identifier as well

--- AWSS3/AWSS3TransferUtility.m
+++ AWSS3/AWSS3TransferUtility.m
@@ -1369,7 +1369,7 @@ static AWSS3TransferUtility *_defaultS3TransferUtility = nil;
         //Start the subTasks
         for(id taskIdentifier in transferUtilityMultiPartUploadTask.inProgressPartsDictionary) {
             AWSS3TransferUtilityUploadSubTask *subTask = [transferUtilityMultiPartUploadTask.inProgressPartsDictionary objectForKey:taskIdentifier];
-            AWSDDLogError(@"Starting subTask %@", @(subTask.taskIdentifier));
+            AWSDDLogError(@"Starting subTask %@, taskIdentifier %@", @(subTask.taskIdentifier), @(taskIdentifier));
             [subTask.sessionTask resume];
         }
         
